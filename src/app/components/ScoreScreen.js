4ad095d9e19d2dfab501eade738efb71b5af8105
tyVespA <migliavacca.marco08@gmail.com>
2023-11-21 05:23:40 +0100

    Create and style

---
+++ src/app/components/ScoreScreen.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./ScoreScreen.module.css";
+import { useState } from "react";
+import { useEffect } from "react";
+
+export default function ScoreScreen({ score }) {
+  const [result, setResult] = useState("");
+
+  useEffect(() => {
+    if (score < 5) {
+      setResult("You classify as a normal human being");
+    } else if (score >= 5 && score < 7) {
+      setResult("You are fairly degen");
+    } else if (score >= 7) {
+      setResult("You are full degen");
+    }
+  }, [score]);
+
+  return (
+    <div>
+      <h2>{`You scored ${score}/10`}</h2>
+      <h2>{result}</h2>
+    </div>
+  );
+}
