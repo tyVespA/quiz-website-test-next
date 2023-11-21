bfd28b8be14c387ff327d3982e799a6c42a44d41
tyVespA <migliavacca.marco08@gmail.com>
2023-11-21 02:31:36 +0100

    Add new component for testing, add css module

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
-import Question from "./components/Question";
 import styles from "./page.module.css";
+import Question from "./components/Question";
+import Quiz from "./components/Quiz";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Question />
+      <Quiz />
     </main>
   );
 }
