2e253f8c3ebe7a1c079620a5bc9b8a516842965e
tyVespA <migliavacca.marco08@gmail.com>
2023-11-21 02:32:05 +0100

    Add props and iteration

--- src/app/components/Question.js
+++ src/app/components/Question.js
@@ -1,22 +1,27 @@
 "use client";
 import React from "react";
+import { useState } from "react";
 import styles from "./Question.module.css";
 
-export default function Question() {
+export default function Question({ question, options }) {
+  const [score, setScore] = useState(0);
+
   return (
     <div className={styles.questionContainer}>
-      <h2>How do you feel about anime?</h2>
+      <h2>{question}</h2>
       <form
         action=""
         className={styles.form}
         onSubmit={(e) => {
           e.preventDefault();
+          console.log("Score:", score);
         }}
       >
-        <button>I'm a huge fan</button>
-        <button>I like some</button>
-        <button>I cum to hentai</button>
-        <button>I don't really like them</button>
+        {options.map((option, index) => (
+          <button key={index} onClick={() => setScore(option.score)}>
+            {option.label}
+          </button>
+        ))}
       </form>
     </div>
   );
