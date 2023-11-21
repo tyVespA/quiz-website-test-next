d535fb01fb3c5fbcd430102ebe2ab744d52b5514
tyVespA <migliavacca.marco08@gmail.com>
2023-11-21 04:43:05 +0100

    Add position relative to questions container

--- src/app/components/Quiz.js
+++ src/app/components/Quiz.js
@@ -1,19 +1,15 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
+import styles from "./Quiz.module.css";
 import Question from "./Question";
 import { questions } from "../assets/questions";
 
 export default function Quiz() {
   const [score, setScore] = useState(0);
 
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-
-  const handleNextQuestion = () => {
-    setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
-  };
-
   return (
-    <div>
+    <div className={styles.quizContainer}>
       {questions.map((question, index) => (
         <Question
           key={index}
@@ -21,14 +17,12 @@ export default function Quiz() {
           options={question.options}
           score={score}
           setScore={setScore}
-          isVisible={index === currentQuestionIndex}
-          onNextQuestion={handleNextQuestion}
         />
       ))}
 
       <div>
         <br />
-        {currentQuestionIndex === questions.length ? <p>Score: {score}</p> : ""}
+        <p>Score: {score}</p>
       </div>
     </div>
   );
