653493c808b1c1ca194384f60dbe5ab89004abf7
tyVespA <migliavacca.marco08@gmail.com>
2023-11-21 02:56:40 +0100

    Move questions to separate file

--- src/app/assets/questions.js
+++ src/app/assets/questions.js
@@ -1,9 +1,23 @@
 export const questions = [
   {
     question: "How do you feel about anime?",
-    option1: "",
-    option2: "",
-    option3: "",
-    option4: "",
+    options: [
+      { label: "I'm a big fan", score: 4 },
+      { label: "I enjoy them", score: 1 },
+      { label: "I like a few", score: 0 },
+      { label: "I cum to hentai", score: 6 },
+      { label: "I don't really like them", score: 0 },
+    ],
+  },
+  {
+    question: "How often do you masturbate?",
+    options: [
+      { label: "Multiple times a day", score: 5 },
+      { label: "Once a day", score: 2 },
+      { label: "A few times a week", score: 0 },
+      { label: "Once a week", score: 0 },
+      { label: "A few times per month", score: 2 },
+      { label: "Almost never", score: 3 },
+    ],
   },
 ];
--- src/app/components/Quiz.js
+++ src/app/components/Quiz.js
@@ -5,30 +5,6 @@ import Question from "./Question";
 import { questions } from "../assets/questions";
 
 export default function Quiz() {
-  const questions = [
-    {
-      question: "How do you feel about anime?",
-      options: [
-        { label: "I'm a big fan", score: 4 },
-        { label: "I enjoy them", score: 1 },
-        { label: "I like a few", score: 0 },
-        { label: "I cum to hentai", score: 6 },
-        { label: "I don't really like them", score: 0 },
-      ],
-    },
-    {
-      question: "How often do you masturbate?",
-      options: [
-        { label: "Multiple times a day", score: 5 },
-        { label: "Once a day", score: 2 },
-        { label: "A few times a week", score: 0 },
-        { label: "Once a week", score: 0 },
-        { label: "A few times per month", score: 2 },
-        { label: "Almost never", score: 3 },
-      ],
-    },
-  ];
-
   const [score, setScore] = useState(0);
 
   return (
