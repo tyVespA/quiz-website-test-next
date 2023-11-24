3f399c414b8f2864a0b172427a6e0579d8e641d5
tyVespA <migliavacca.marco08@gmail.com>
2023-11-24 18:04:48 +0100

    Update global a, change class name

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,10 @@ import Quiz from "./components/Quiz";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Quiz />
+    <main>
+      <div className={styles.quizContent}>
+        <Quiz />
+      </div>
     </main>
   );
 }
