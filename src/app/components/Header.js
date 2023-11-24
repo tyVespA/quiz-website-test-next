8fa385672fc02c30671043670fb895638bfd7807
tyVespA <migliavacca.marco08@gmail.com>
2023-11-24 19:25:33 +0100

    Create, style, fix layout to fit header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -5,30 +5,29 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <Link href={"/"} className={styles.logo}>
-        <Image
-          src={"./images/logo-white.svg"}
-          width={65}
-          height={65}
-          alt={"logo"}
-        />
-        <p className={styles.title}>DrK Tests</p>
-      </Link>
-
-      <nav>
-        <ul>
-          <li>
-            <Link href={"/"}>Home</Link>
-          </li>
-          <li>
-            <Link href={"/"}>Tests</Link>
-          </li>
-          <li>
-            <Link href={"/"}>Contact</Link>
-          </li>
-        </ul>
-      </nav>
+    <header className={styles.headerContainer}>
+      <div className={styles.header}>
+        <Link href={"/"} className={styles.logo}>
+          <Image src={"./logo-white.svg"} width={60} height={60} alt={"logo"} />
+          <p className={styles.title}>Embrace Mental Illness</p>
+        </Link>
+        <nav>
+          <ul>
+            <li>
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/"}>About</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Tests</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
