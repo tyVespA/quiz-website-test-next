9f14ffcf60368a3d607671863b5b57a782195469
tyVespA <migliavacca.marco08@gmail.com>
2023-11-25 03:00:08 +0100

    Add button style

--- src/app/page.js
+++ src/app/page.js
@@ -50,7 +50,7 @@ export default function Home() {
             <TestCard
               imageName={"erobb-pain.gif"}
               alt={"man drinking"}
-              title={"Do you think you might be a degenerate?"}
+              title={"Am I a cuck?"}
               description={"Find out if you're a disgrace to society."}
             />
           </div>
