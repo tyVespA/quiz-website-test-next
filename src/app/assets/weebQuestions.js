c769fa6fc3cff46ff4955f947b34af4c29c0c897
tyVespA <migliavacca.marco08@gmail.com>
2023-11-27 03:29:54 +0100

    Add question samples

---
+++ src/app/assets/weebQuestions.js
@@ -0,0 +1,20 @@
+export const weebTest = [
+  {
+    question: "How often do you watch anime?",
+    answers: [
+      { label: "1", points: "0" },
+      { label: "1", points: "0" },
+      { label: "1", points: "0" },
+      { label: "1", points: "0" },
+    ],
+  },
+  {
+    question: "How often do you watch anime?",
+    answers: [
+      { label: "1", points: "0" },
+      { label: "1", points: "0" },
+      { label: "1", points: "0" },
+      { label: "1", points: "0" },
+    ],
+  },
+];
