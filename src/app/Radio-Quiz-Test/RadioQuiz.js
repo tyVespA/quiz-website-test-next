93f4f7c02a1a62154f8097d45ebc2451c1016f89
tyVespA <migliavacca.marco08@gmail.com>
2023-11-29 01:11:29 +0100

    Add map function

--- src/app/Radio-Quiz-Test/RadioQuiz.js
+++ src/app/Radio-Quiz-Test/RadioQuiz.js
@@ -3,17 +3,26 @@ import React from "react";
 import { useState } from "react";
 import RadioQuestion from "./RadioQuestion";
 import styles from "./RadioQuiz.module.css";
-import { quiz } from "./quiz";
 
 export default function RadioQuiz({ quiz }) {
+  const questionsAmount = quiz.length;
+  console.log(questionsAmount);
+
   const [scores, setScores] = useState({
     Q1: 0,
     Q2: 0,
     Q3: 0,
+    Q4: 0,
+    Q5: 0,
+    Q6: 0,
+    Q7: 0,
+    Q8: 0,
+    Q9: 0,
+    Q10: 0,
   });
 
   const [formSubmitted, setFormSubmitted] = useState(false);
-  const [submitQ1, setSubmitQ1] = useState(false);
+  // const [submitQ1, setSubmitQ1] = useState(false);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -35,21 +44,27 @@ export default function RadioQuiz({ quiz }) {
 
         {scores.Q1 <= 0 && <p>Select an answer to display the next question</p>}
 
-        <div
-          className={`${styles.questionContainer} ${
-            scores.Q1 > 0 ? styles.fadeIn : ""
-          }  `}
-        >
-          <RadioQuestion
-            question={quiz[1].question}
-            answers={quiz[1].answers}
-            name="Q2"
-            score={scores.Q2}
-            setScore={(newScore) =>
-              setScores((prevScores) => ({ ...prevScores, Q2: newScore }))
-            }
-          ></RadioQuestion>
-        </div>
+        {quiz.map((question, index) => (
+          <div
+            key={index}
+            className={`${styles.questionContainer} ${
+              scores[`Q${index + 1}`] > 0 ? styles.fadeIn : ""
+            }  `}
+          >
+            <RadioQuestion
+              question={question.question}
+              answers={question.answers}
+              name={`Q${index + 1}`}
+              score={scores[`Q${index + 1}`]}
+              setScore={(newScore) =>
+                setScores((prevScores) => ({
+                  ...prevScores,
+                  [`Q${index + 1}`]: newScore,
+                }))
+              }
+            ></RadioQuestion>
+          </div>
+        ))}
 
         <button
           type="submit"
@@ -57,11 +72,21 @@ export default function RadioQuiz({ quiz }) {
             scores.Q2 > 0 ? styles.fadeIn : ""
           }`}
         >
-          Calculate Score
+          Calculate Result
         </button>
 
         <p className={`${styles.score} ${formSubmitted ? styles.fadeIn : ""}`}>
-          Score: {scores.Q1 + scores.Q2 + scores.Q3}
+          Score:{" "}
+          {scores.Q1 +
+            scores.Q2 +
+            scores.Q3 +
+            scores.Q4 +
+            scores.Q5 +
+            scores.Q6 +
+            scores.Q7 +
+            scores.Q8 +
+            scores.Q9 +
+            scores.Q10}
         </p>
       </form>
       <br />
