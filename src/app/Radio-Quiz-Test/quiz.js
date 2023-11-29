0649d50fe3e6de9c01034b03465111dd16c94934
tyVespA <migliavacca.marco08@gmail.com>
2023-11-29 01:11:14 +0100

    Add test questions

--- src/app/Radio-Quiz-Test/quiz.js
+++ src/app/Radio-Quiz-Test/quiz.js
@@ -9,7 +9,43 @@ export const quiz = [
     ],
   },
   {
-    question: "1. what is the asdjasikdha",
+    question: "2. what is the asdjasikdha",
+    answers: [
+      { label: "123", score: 2 },
+      { label: "zxcbff", score: 1 },
+      { label: "12xzczx3", score: 4 },
+      { label: "czxvq123e", score: 5 },
+    ],
+  },
+  {
+    question: "3. QS",
+    answers: [
+      { label: "qs2", score: 2 },
+      { label: "qs3", score: 1 },
+      { label: "qs4", score: 4 },
+      { label: "qs45", score: 5 },
+    ],
+  },
+  {
+    question: "4. what is the asdjasikdha",
+    answers: [
+      { label: "123", score: 2 },
+      { label: "zxcbff", score: 1 },
+      { label: "12xzczx3", score: 4 },
+      { label: "czxvq123e", score: 5 },
+    ],
+  },
+  {
+    question: "5. QS",
+    answers: [
+      { label: "qs2", score: 2 },
+      { label: "qs3", score: 1 },
+      { label: "qs4", score: 4 },
+      { label: "qs45", score: 5 },
+    ],
+  },
+  {
+    question: "6. what is the asdjasikdha",
     answers: [
       { label: "123", score: 2 },
       { label: "zxcbff", score: 1 },
