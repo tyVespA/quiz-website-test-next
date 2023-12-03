bac78c1c6e8785ea13e43a1a5f63f1b7ef430bb6
tyVespA <migliavacca.marco08@gmail.com>
2023-12-03 04:00:19 +0100

    Remove imports

--- src/app/tests/cuck-test/page.js
+++ src/app/tests/cuck-test/page.js
@@ -5,10 +5,7 @@ export default function page() {
   return (
     <div className="test-page">
       <h1>cuck test</h1>
-      <div className="quiz-container">
-        <FadeInRadioQuiz testName={weebQuestions} questionIndex={0} />
-        <FadeInRadioQuiz testName={weebQuestions} questionIndex={1} />
-      </div>
+      <div className="quiz-container"></div>
       <div>Score: </div>
     </div>
   );
